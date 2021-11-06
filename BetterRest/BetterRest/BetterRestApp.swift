d311c5429b7635417ac64c62a8599e094af42678
Michael VanDyke <michael.vandyke@twinspires.com>
2021-11-06 19:39:03 -0400

    Add ViewsAndModifiers and BetterRest projects

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Michael VanDyke on 11/6/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
